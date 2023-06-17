e53fcb9e04c48851c65c6431c821a5a177df08b4
arushisharma <39919147+1234arushi@users.noreply.github.com>
2023-06-17 13:37:12 +0530

    Update frogjump.cpp

--- frogjump.cpp
+++ frogjump.cpp
@@ -1,3 +1,28 @@
+//tabulation
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    //tabulation method:
+    vector<int>dp (n, 0);
+    dp[0]=0;
+    for(int i=1;i<n;i++)
+    {
+        int fs=dp[i-1]+abs(heights[i]-heights[i-1]);
+        int ss=INT_MAX;
+        if(i>1)
+        {
+            ss=dp[i-2]+abs(heights[i]-heights[i-2]);
+
+        }
+        dp[i]=min(fs,ss);
+        
+    }
+    return dp[n-1];
+
+    // Write your code here.
+}
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//memoization
 #include <bits/stdc++.h>
 int solve(int indx,vector<int>heights,vector<int>&dp)
 {
